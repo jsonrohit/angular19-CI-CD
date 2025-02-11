104c8e835534eb3d802cd1b441eefdea8b747b5d
jsonrohit <jsonrohit@users.noreply.github.com>
2025-02-11 08:50:41 +0000

    deploy: 14b9809420a32eca285b437d2d190bdb3fe9f8b5

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 496, hash: 'a0a1d7b3dffa86b840c6c2a1ca7addc9e14447263e2482a79973d4677af3bb5b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1009, hash: 'd7da7ab1200357e4199aac5e50483aab1a8dfc4e496dec699b7cc7046b7b54f1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 14999, hash: 'f5003369b1054097581611715b9615ea811475760407bcd1f883b5e9e202fe7b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 496, hash: '4145c90ec50415356fed530cd519a399344df68590291fa13e6b2b60243e881e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1009, hash: '21370fa08440395c9aa9ea7a410dd7020645e84bb4de1cf237b1d8cbbd49e146', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 12823, hash: '737eeb6c606b48d626c5e40498d833a7dd3e7089ac0be949abf684d948a376e4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -9,6 +9,6 @@ export default `<!doctype html>
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-G556DH7X.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-ALAQTK4U.js" type="module"></script></body>
 </html>
 `;
