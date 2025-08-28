6badaba4ff4705c776719bf52ff82fea61d79664
jsonrohit <jsonrohit@users.noreply.github.com>
2025-08-28 18:34:25 +0000

    deploy: 3d1cb8f1204c1acb5ae1b1c100668ac9be33f08d

--- server/assets-chunks/index_server_html.mjs
+++ server/assets-chunks/index_server_html.mjs
@@ -11,4 +11,6 @@ export default `<!doctype html>
 </script>
   <app-root></app-root>
 <script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-ALAQTK4U.js" type="module"></script></body>
-</html>`;
+</html>
+
+`;
